27f187e7f636d4e1f87375e578fe0c23ee883fc5
Moritz Wallawisch <wallawitsch@gmail.com>
2020-05-12 12:45:07 +0200

    init and setup graphjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import { Line } from "react-chartjs-2"
+
+import "./App.css"
 
 function App() {
+  const [chartData, setChartData] = useState({})
+
+  const chart = () => {
+    setChartData({
+      labels: ["1", "2", "3", "4", "5"],
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: "rgba(52, 152, 219, 0.75)",
+          data: [5, 10, 15, 30, 50],
+        },
+        {
+          label: "My Second dataset",
+          backgroundColor: "rgba(231, 76, 60, 0.75)",
+          data: [300, 500, 100, 40, 120],
+        },
+      ],
+    })
+  }
+
+  useEffect(() => {
+    chart()
+  }, [])
+
+  console.log(chartData)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Line
+        data={chartData}
+        options={{
+          title: {
+            display: true,
+            text: "Chart.js Toggle with React",
+            fontSize: 20,
+          },
+          scales: {
+            xAxes: [
+              {
+                display: false,
+              },
+            ],
+            yAxes: [
+              {
+                type: "linear",
+                display: true,
+                position: "left",
+              },
+            ],
+          },
+        }}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
